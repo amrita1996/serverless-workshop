430e919352b6f27f136b6d75dd3ba6b90bdd3a76
Amrita Krishna <am242705@dal.ca>
2022-03-22 20:00:47 -0300

    refactor

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  basePath: "https://623a4d1a0c1e3e40bc7ce94f--jazzy-maamoul-8cc6a8.netlify.app/.netlify/functions",
+  basePath: "https://623a4d1a0c1e3e40bc7ce94f--jazzy-maamoul-8cc6a8.netlify.app/.netlify/functions/",
   profilePath: "profile-data",
   i18nKeyStorePath: "i18n-keystore"
 };
